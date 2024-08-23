692c7551d2a62f54957eefad6f82bf23c81b343d
LDG12 <person456@naver.com>
2024-08-23 18:45:26 +0900

    fix: User Controller의 반환 타입 변경( Response Entity로 )

--- gagyebbyu/src/main/java/com/bbyuworld/gagyebbyu/domain/user/controller/UserController.java
+++ gagyebbyu/src/main/java/com/bbyuworld/gagyebbyu/domain/user/controller/UserController.java
@@ -7,6 +7,7 @@ import com.bbyuworld.gagyebbyu.global.jwt.UserContext;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -25,33 +26,32 @@ public class UserController {
      * @return
      */
     @PostMapping("/regist")
-    public String regist(@RequestBody UserDto user) throws JsonProcessingException {
+    public ResponseEntity<String> regist(@RequestBody UserDto user) throws JsonProcessingException {
         System.out.println("User = "+user);
         userService.regist(user);
 //        userService.test(user.getEmail());
-        return "hello";
+        return ResponseEntity.ok("회원가입 성공");
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody UserDto user){
+    public ResponseEntity<String> login(@RequestBody UserDto user){
         boolean flag = userService.login(user);
-        return "no";
+        return ResponseEntity.ok("로그인 성공");
     }
 
     @PostMapping("/logout")
     @RequireJwtToken
-    public String logout(){
+    public ResponseEntity<String> logout(){
         Long userId = UserContext.getUserId();
         userService.logout(userId);
-        System.out.println("userId = "+userId);
-        return "logout";
+        return ResponseEntity.ok("로그아웃 성공");
     }
 
     @DeleteMapping("/delete")
     @RequireJwtToken
-    public String deleteUser(){
+    public ResponseEntity<String> deleteUser(){
         Long userId = UserContext.getUserId();
         userService.deleteUser(userId);
-        return "delete";
+        return ResponseEntity.ok("계정 삭제 성공");
     }
 }
