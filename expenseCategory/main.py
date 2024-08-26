bbb4fd41f4cd8bd5ae6401d16505f36535835b1c
justice <definition0215@gmail.com>
2024-08-26 16:40:02 +0900

    fix: 카테고리 결과 포맷 변경

--- expenseCategory/main.py
+++ expenseCategory/main.py
@@ -7,10 +7,8 @@ import pandas as pd
 from model.inference_streamlit import load_model, inference
 from model.modules.preprocess import preprocess_infer
 
-# 디바이스 설정 (CUDA 또는 CPU)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# FastAPI 애플리케이션 설정
 app = FastAPI()
 
 # KoBERT 모델과 토크나이저 로드
@@ -31,10 +29,10 @@ def inference_single(inp: InferenceData):
 
     try:
         # 모델과 토크나이저 설정
-        cate, prob = inference(preprocess_infer(merch_name), 26, MODEL, TOKENIZER, device, topk=True)
+        cate, prob = inference(preprocess_infer(merch_name), 26, MODEL, TOKENIZER, device, topk=False)
 
         # 결과를 DataFrame으로 변환 및 JSON으로 반환
-        result = pd.DataFrame({'업종': cate.flatten(), '예측확률': prob.flatten()})
+        result = pd.DataFrame({'category': [cate], 'predictionProbability': [prob]})
         result_json = result.to_json(orient='records')
         return JSONResponse(content=json.loads(result_json))
     except Exception as e:
