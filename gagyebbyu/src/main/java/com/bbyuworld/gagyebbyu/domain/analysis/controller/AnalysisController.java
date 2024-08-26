dc3b2857c90b68f546bd948463a065cb40d23954
seojiheun <wlgmsaudqls@naver.com>
2024-08-27 00:43:07 +0900

    feat: Analysis Expense Result 조회

---
+++ gagyebbyu/src/main/java/com/bbyuworld/gagyebbyu/domain/analysis/controller/AnalysisController.java
@@ -0,0 +1,27 @@
+package com.bbyuworld.gagyebbyu.domain.analysis.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bbyuworld.gagyebbyu.domain.analysis.dto.response.CoupleExpenseResultDto;
+import com.bbyuworld.gagyebbyu.domain.analysis.service.AnalysisService;
+import com.bbyuworld.gagyebbyu.global.jwt.RequireJwtToken;
+import com.bbyuworld.gagyebbyu.global.jwt.UserContext;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/analysis")
+@RequiredArgsConstructor
+public class AnalysisController {
+	private final AnalysisService analysisService;
+
+	@GetMapping(path = "/couple-expense/result", produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequireJwtToken
+	public ResponseEntity<CoupleExpenseResultDto> getCoupleExpenseResult() {
+		return ResponseEntity.ok(analysisService.getCoupleExpenseResult(UserContext.getUserId()));
+	}
+}
