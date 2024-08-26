3db6202969bfc5a8d1a8573f306f1452452301ca
임동길 <person456@naver.com>
2024-08-26 11:27:48 +0900

    feat: 메인화면 구현

--- gagyebbyu/src/main/java/com/bbyuworld/gagyebbyu/domain/user/service/UserService.java
+++ gagyebbyu/src/main/java/com/bbyuworld/gagyebbyu/domain/user/service/UserService.java
@@ -1,10 +1,16 @@
 package com.bbyuworld.gagyebbyu.domain.user.service;
 
+import com.bbyuworld.gagyebbyu.domain.asset.entity.Asset;
+import com.bbyuworld.gagyebbyu.domain.asset.entity.AssetDeposit;
+import com.bbyuworld.gagyebbyu.domain.asset.entity.Type;
 import com.bbyuworld.gagyebbyu.domain.user.dto.LoginResponseDto;
+import com.bbyuworld.gagyebbyu.domain.user.dto.UserAccountRequestDto;
 import com.bbyuworld.gagyebbyu.domain.user.dto.UserDto;
 import com.bbyuworld.gagyebbyu.domain.user.dto.UserResponseDto;
+import com.bbyuworld.gagyebbyu.domain.user.entity.Gender;
 import com.bbyuworld.gagyebbyu.domain.user.entity.User;
 import com.bbyuworld.gagyebbyu.domain.user.repository.UserRepository;
+import com.bbyuworld.gagyebbyu.global.api.demanddeposit.AccountDto;
 import com.bbyuworld.gagyebbyu.global.error.ErrorCode;
 import com.bbyuworld.gagyebbyu.global.error.type.BadRequestException;
 import com.bbyuworld.gagyebbyu.global.error.type.UserNotFoundException;
@@ -27,6 +33,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -97,14 +104,52 @@ public class UserService {
         return false;
     }
 
-    public void test(String userEmail) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        ObjectNode objectNode = objectMapper.createObjectNode();
-        objectNode.put("apiKey", apiKey);
-        objectNode.put("userId", userEmail);
-        String json = objectMapper.writeValueAsString(objectNode);
-        System.out.println("결과 = "+json);
+    @Transactional
+    public boolean saveAdditionalInfo(Long userId, UserAccountRequestDto dto){
+        User user = userRepository.findUserById(userId);
+        if(dto.getAdditionalInfo().getGender().equals("M")){
+            user.setGender(Gender.M);
+        }
+        else{
+            user.setGender(Gender.F);
+        }
+        user.setAge(dto.getAdditionalInfo().getAge());
+        Long monthlyIncome = dto.getAdditionalInfo().getSalary() / 12L;
+        user.setMonthlyIncome(monthlyIncome);
+        user.setMonthlyTargetAmount((long) dto.getAdditionalInfo().getDesiredSpending());
+
+        for(AccountDto accountDto : dto.getSelectedAccounts()){
+            Asset asset = new Asset();
+            if(accountDto.getAccountTypeName().equals("수시입출금"))
+                asset.setType(Type.DEPOSIT);
+            asset.setBankName(accountDto.getBankName());
+            asset.setAmount(accountDto.getAccountBalance());
+            asset.setCreatedAt(LocalDateTime.now());
+            asset.setUpdatedAt(LocalDateTime.now());
+            asset.setUser(user);
+            AssetDeposit assetDeposit = new AssetDeposit();
+            assetDeposit.setBank(accountDto.getBankName());
+            assetDeposit.setBankCode(accountDto.getBankCode());
+            assetDeposit.setNumber(accountDto.getAccountNo());
+            assetDeposit.setType(accountDto.getAccountTypeName());
+            assetDeposit.setAmount(accountDto.getAccountBalance());
+            assetDeposit.setHidden(false);
+            asset.setAssetDeposit(assetDeposit);
+            assetDeposit.setAsset(asset);
+            assetDeposit.setOneTimeTransferLimit(accountDto.getOneTimeTransferLimit());
+            assetDeposit.setDailyTransferLimit(accountDto.getDailyTransferLimit());
+            user.getAssets().add(asset);
+        }
+        userRepository.save(user);
+        return true;
     }
+
+
+
+
+
+
+
 
 
     private String createUserKey(String userEmail) throws IOException {
