da218a590551788987699c7cf2162475f335e54b
seojiheun <wlgmsaudqls@naver.com>
2024-08-31 14:31:02 +0900

    feat: Couple 총 자산 조회

--- gagyebbyu/src/main/java/com/bbyuworld/gagyebbyu/domain/asset/controller/AssetController.java
+++ gagyebbyu/src/main/java/com/bbyuworld/gagyebbyu/domain/asset/controller/AssetController.java
@@ -1,79 +1,91 @@
 package com.bbyuworld.gagyebbyu.domain.asset.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.bbyuworld.gagyebbyu.domain.asset.dto.AssetAccountDto;
 import com.bbyuworld.gagyebbyu.domain.asset.dto.AssetCardDto;
 import com.bbyuworld.gagyebbyu.domain.asset.dto.AssetDto;
 import com.bbyuworld.gagyebbyu.domain.asset.dto.AssetLoanDto;
+import com.bbyuworld.gagyebbyu.domain.asset.dto.AssetResponseDto;
 import com.bbyuworld.gagyebbyu.domain.asset.service.asset.AssetService;
 import com.bbyuworld.gagyebbyu.global.jwt.RequireJwtToken;
 import com.bbyuworld.gagyebbyu.global.jwt.UserContext;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/assets")
 public class AssetController {
-    private final AssetService assetService;
+	private final AssetService assetService;
 
-    @GetMapping("/user")
-    @RequireJwtToken
-    public List<AssetDto> getUserAssets() {
-        return assetService.getAssets(UserContext.getUserId());
-    }
+	@GetMapping("/user")
+	@RequireJwtToken
+	public List<AssetDto> getUserAssets() {
+		return assetService.getAssets(UserContext.getUserId());
+	}
 
-    @GetMapping("/user/sum")
-    @RequireJwtToken
-    public Long getUserAssetSum() {
-        return assetService.getSumUserAssets(UserContext.getUserId());
-    }
+	@GetMapping("/user/sum")
+	@RequireJwtToken
+	public Long getUserAssetSum() {
+		return assetService.getSumUserAssets(UserContext.getUserId());
+	}
 
+	@GetMapping("/couple")
+	@RequireJwtToken
+	public List<AssetDto> getCoupleAssets() {
+		return assetService.getCoupleAssets(UserContext.getUserId());
+	}
 
-    @GetMapping("/couple")
-    @RequireJwtToken
-    public List<AssetDto> getCoupleAssets() {
-        return assetService.getCoupleAssets(UserContext.getUserId());
-    }
+	@GetMapping("/couple/main")
+	@RequireJwtToken
+	public AssetResponseDto getCoupleAssetsWithoutLoan() {
+		return assetService.getCoupleAssetsWithoutLoan(UserContext.getUserId());
+	}
 
-    @GetMapping("/couple/sum")
-    @RequireJwtToken
-    public Long getCoupleAssetSum() {
-        return assetService.getSumCoupleAssets(UserContext.getUserId());
-    }
+	@GetMapping("/couple/sum")
+	@RequireJwtToken
+	public Long getCoupleAssetSum() {
+		return assetService.getSumCoupleAssets(UserContext.getUserId());
+	}
 
-    @GetMapping("/couple/account/sum")
-    @RequireJwtToken
-    public Long getCoupleAssetAccountSum() {
-        Long userId = UserContext.getUserId();
-        return assetService.getSumCoupleAccountAssets(userId);
-    }
+	@GetMapping("/couple/account/sum")
+	@RequireJwtToken
+	public Long getCoupleAssetAccountSum() {
+		Long userId = UserContext.getUserId();
+		return assetService.getSumCoupleAccountAssets(userId);
+	}
 
-    @PatchMapping("/{assetId}/visibility")
-    public void updateAssetVisibility(@PathVariable Long assetId, @RequestParam("isHidden") boolean isHidden) {
-        assetService.updateAssetVisibility(assetId, isHidden);
-    }
+	@PatchMapping("/{assetId}/visibility")
+	public void updateAssetVisibility(@PathVariable Long assetId, @RequestParam("isHidden") boolean isHidden) {
+		assetService.updateAssetVisibility(assetId, isHidden);
+	}
 
-    @PostMapping("/add-loan")
-    @RequireJwtToken
-    public boolean addNewLoan(@RequestBody AssetLoanDto assetLoanDto) {
-        return assetService.insertNewLoan(assetLoanDto, UserContext.getUserId());
-    }
+	@PostMapping("/add-loan")
+	@RequireJwtToken
+	public boolean addNewLoan(@RequestBody AssetLoanDto assetLoanDto) {
+		return assetService.insertNewLoan(assetLoanDto, UserContext.getUserId());
+	}
 
-    @PostMapping("/add-account")
-    @RequireJwtToken
-    public boolean addNewAccount(@RequestBody AssetAccountDto assetAccountDto) {
-        return assetService.insertNewAccount(assetAccountDto, UserContext.getUserId());
-    }
+	@PostMapping("/add-account")
+	@RequireJwtToken
+	public boolean addNewAccount(@RequestBody AssetAccountDto assetAccountDto) {
+		return assetService.insertNewAccount(assetAccountDto, UserContext.getUserId());
+	}
 
-    @PostMapping("/add-card")
-    @RequireJwtToken
-    public boolean addNewCard(@RequestBody AssetCardDto assetCardDto) {
-        return assetService.insertNewCard(assetCardDto, UserContext.getUserId());
-    }
-
-
+	@PostMapping("/add-card")
+	@RequireJwtToken
+	public boolean addNewCard(@RequestBody AssetCardDto assetCardDto) {
+		return assetService.insertNewCard(assetCardDto, UserContext.getUserId());
+	}
 
 }
--- gagyebbyu/src/main/java/com/bbyuworld/gagyebbyu/domain/asset/service/asset/AssetService.java
+++ gagyebbyu/src/main/java/com/bbyuworld/gagyebbyu/domain/asset/service/asset/AssetService.java
@@ -1,35 +1,38 @@
 package com.bbyuworld.gagyebbyu.domain.asset.service.asset;
 
+import java.util.List;
+
 import com.bbyuworld.gagyebbyu.domain.asset.dto.AssetAccountDto;
 import com.bbyuworld.gagyebbyu.domain.asset.dto.AssetCardDto;
 import com.bbyuworld.gagyebbyu.domain.asset.dto.AssetDto;
 import com.bbyuworld.gagyebbyu.domain.asset.dto.AssetLoanDto;
-import com.bbyuworld.gagyebbyu.domain.asset.entity.Asset;
+import com.bbyuworld.gagyebbyu.domain.asset.dto.AssetResponseDto;
 import com.bbyuworld.gagyebbyu.domain.couple.entity.Couple;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface AssetService {
-    List<AssetDto> getAssets(Long userId);
+	List<AssetDto> getAssets(Long userId);
 
-    Long getSumUserAssets(Long userId);
+	Long getSumUserAssets(Long userId);
 
+	AssetResponseDto getCoupleAssetsWithoutLoan(Long userId);
 
-    List<AssetDto> getCoupleAssets(Long userId);
+	List<AssetDto> getCoupleAssets(Long userId);
 
-    Long getSumCoupleAssets(Long userId);
+	Long getSumCoupleAssets(Long userId);
 
-    void updateAssetVisibility(Long assetId, boolean isHidden);
+	void updateAssetVisibility(Long assetId, boolean isHidden);
 
-    void updateUserAssetsToCouple(Couple couple, Long user1Id, Long user2Id);
-    Long getSumCoupleAccountAssets(Long userId);
-    /* INSERT 이 있어야 함 */
-    boolean insertNewLoan(AssetLoanDto assetLoanDtoDto, Long userId);
-    boolean insertNewCard(AssetCardDto assetCardDto, Long userId);
-    boolean insertNewAccount(AssetAccountDto assetAccountDto, Long userId);
+	void updateUserAssetsToCouple(Couple couple, Long user1Id, Long user2Id);
 
-    /* 주택담보대출의 총 액 반환 */
-    long getSumMortgage(Long userId);
+	Long getSumCoupleAccountAssets(Long userId);
+
+	/* INSERT 이 있어야 함 */
+	boolean insertNewLoan(AssetLoanDto assetLoanDtoDto, Long userId);
+
+	boolean insertNewCard(AssetCardDto assetCardDto, Long userId);
+
+	boolean insertNewAccount(AssetAccountDto assetAccountDto, Long userId);
+
+	/* 주택담보대출의 총 액 반환 */
+	long getSumMortgage(Long userId);
 }
