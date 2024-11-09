1eee85b1bef3ea2b8013d4bd0be6334313082c24
seojiheun <wlgmsaudqls@naver.com>
2024-11-09 22:51:58 +0900

    rename: CoupleExpenseService 이름 변경

--- gagyebbyu/src/main/java/com/bbyuworld/gagyebbyu/domain/analysis/controller/AnalysisExpenseController.java
+++ gagyebbyu/src/main/java/com/bbyuworld/gagyebbyu/domain/analysis/controller/AnalysisExpenseController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bbyuworld.gagyebbyu.domain.analysis.dto.param.AnalysisParam;
 import com.bbyuworld.gagyebbyu.domain.analysis.dto.response.CoupleExpenseResultDto;
 import com.bbyuworld.gagyebbyu.domain.analysis.dto.response.CoupleExpenseStatisticsDto;
-import com.bbyuworld.gagyebbyu.domain.analysis.service.CoupleExpenseService;
+import com.bbyuworld.gagyebbyu.domain.analysis.service.AnalysisExpenseService;
 import com.bbyuworld.gagyebbyu.global.jwt.RequireJwtToken;
 import com.bbyuworld.gagyebbyu.global.jwt.UserContext;
 
@@ -22,7 +22,7 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/analysis/couple-expense")
 @RequiredArgsConstructor
 public class AnalysisExpenseController {
-	private final CoupleExpenseService coupleExpenseService;
+	private final AnalysisExpenseService analysisExpenseService;
 
 	/**
 	 * 최종 소비 패턴 분석(최다 소비 카테고리, 타 부부 평균 소비량(연령, 나이에 맞는), 우리 부부 소비량, 우리 커플 평균 나이
@@ -31,7 +31,7 @@ public class AnalysisExpenseController {
 	@GetMapping(path = "/result", produces = MediaType.APPLICATION_JSON_VALUE)
 	@RequireJwtToken
 	public ResponseEntity<CoupleExpenseResultDto> getCoupleExpenseResult(@ModelAttribute AnalysisParam param) {
-		return ResponseEntity.ok(coupleExpenseService.getCoupleExpenseResult(UserContext.getUserId(), param));
+		return ResponseEntity.ok(analysisExpenseService.getCoupleExpenseResult(UserContext.getUserId(), param));
 	}
 
 	/**
@@ -42,7 +42,7 @@ public class AnalysisExpenseController {
 	@RequireJwtToken
 	public ResponseEntity<List<CoupleExpenseStatisticsDto>> getCoupleExpenseStatistics(
 		@ModelAttribute AnalysisParam param) {
-		return ResponseEntity.ok(coupleExpenseService.getCoupleExpenseStatistics(UserContext.getUserId(), param));
+		return ResponseEntity.ok(analysisExpenseService.getCoupleExpenseStatistics(UserContext.getUserId(), param));
 	}
 
 }
